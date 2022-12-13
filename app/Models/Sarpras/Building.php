2195be965023bdd4319f6950567d40fdf03ac19d
team-linecode <linecode.team@gmail.com>
2022-12-13 12:35:48 +0700

    CRUD Building R2

---
+++ app/Models/Sarpras/Building.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\Sarpras;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Building extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'stage'];
+}
---
+++ resources/views/manage/sarpras/building/index.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.manage', ['title' => 'Building'])
+
+@section('content')
+    <div class="card">
+        <div class="card-header">
+            <div class="d-flex flex-column flex-sm-row flex-md-row align-items-md-center justify-content-between">
+                <div>
+                    <h4 class="card-title text-center text-lg-start text-uppercase mb-2 mb-md-0 mb-lg-0">Gedung</h4>
+                    <p class="mb-lg-0">Terdapat role default yang tidak dapat dihapus</p>
+                </div>
+                <div class="text-center">
+                    <a href="{{ route('app.building.create') }}" class="btn btn-primary">Tambah</a>
+                </div>
+            </div>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive table-card">
+                <table class="table align-middle w-100 mb-0 dt-serverside">
+                    <thead class="table-light">
+                        <tr>
+                            <th scope="col">No.</th>
+                            <th scope="col">Nama</th>
+                            <th scope="col">Hak Akses</th>
+                            <th scope="col">Opsi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($buildings as $building)
+                            <tr>
+                                <td>{{ $loop->iteration }}</td>
+                                <td>{{ ucwords($building->name) }}</td>
+                                <td>{{ $building->stage }}</td>
+                                <td>
+                                    <div class="d-flex gap-2">
+                                        <div class="edit">
+                                            <a href="{{ route('app.building.edit', $building->id) }}"
+                                                class="btn btn-sm btn-success">Edit</a>
+                                        </div>
+                                        <div class="remove">
+                                            <form action="{{ route('app.building.destroy', $building->id) }}" method="post">
+                                                @csrf
+                                                @method('delete')
+                                                <button type="button" class="btn btn-sm btn-danger c-delete">Hapus</button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+                <!-- end table -->
+            </div>
+            <!-- end table responsive -->
+        </div>
+    </div>
+@stop
